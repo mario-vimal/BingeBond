ef361982c82bf40afdee141fa7e05841cde057a0
Vivek <satsvivek@gmail.com>
2019-11-30 12:54:15 +0530

    Created routes for Login and Register

--- backend/models/user_detail.go
+++ backend/models/user_detail.go
@@ -5,12 +5,17 @@ import (
 )
 
 type UserDetail struct {
-	UserId        int32  `gorm:"primary_key;AUTO_INCREMENT;not null"`
-	Email         string `gorm:"not null;type:varchar(100);unique"`
-	FullName      string `gorm:"not null"`
+	UserId        int32  `gorm:"primary_key;AUTO_INCREMENT;not null" json:"id" binding:"required"`
+	Email         string `gorm:"not null;type:varchar(100);unique" json:"email" binding:"required"`
+	FullName      string `gorm:"not null" json:"fullname" binding:"required"`
 	UserToken     string
-	Password      string    `gorm:"not null"`
-	ContactNumber string    `gorm:"not null;unique"`
-	CreatedAt     time.Time `gorm:"not null"`
-	UpdatedAt     time.Time `gorm:"not null"`
+	Password      string    `gorm:"not null" json:"password" binding:"required"`
+	ContactNumber string    `gorm:"not null;unique" json:"contactnumber" binding:"required"`
+	CreatedAt     time.Time `gorm:"not null" json:"created_at" binding:"required"`
+	UpdatedAt     time.Time `gorm:"not null" json:"updated_at" binding:"required"`
+}
+
+type Loginmodel struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
